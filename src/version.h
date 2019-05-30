5b47aad7a50fb60c1b762b619349f535dc4faa65
XgamingUP{ <xgamingup@gmail.com>
2019-05-30 12:47:13 +0300

    Version bump

--- src/version.h
+++ src/version.h
@@ -13,7 +13,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70920;
+static const int PROTOCOL_VERSION = 70921;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -23,7 +23,7 @@ static const int INIT_PROTO_VERSION = 209;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 70920;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70920;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70921;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
